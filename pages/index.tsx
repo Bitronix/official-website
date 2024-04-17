3060c44f5f35a7fecb3f5e782e15817e45a50ead
“Oborahan” <oborahan@gmail.com>
2024-04-17 18:55:41 +0100

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,19 +4,13 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Cottonly Special - Premium Cotton Wear</title>
-        <meta
-          name="description"
-          content="Explore Cottonly Special's exclusive range of premium cotton wear. Comfort, style, and sustainability in every thread."
-        />
-        <meta
-          name="keywords"
-          content="Cotton Wear, Premium Cotton, Sustainable Fashion, Comfortable Clothing, Eco-Friendly Apparel"
-        />
+        <title>Bitronix</title>
+        <meta name="description" content="" />
+        <meta name="keywords" content="" />
       </Head>
       <div className="flex h-screen justify-center items-center">
         <div className="text-center">
-          <h1 className="text-6xl font-bold">Cottonly UK</h1>
+          <h1 className="text-6xl font-bold">Bitronix</h1>
           <p className="text-xl mt-2">Loading...</p>
         </div>
       </div>
