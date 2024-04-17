0349026f10886161f65661ada4d20e20afb22108
“Oborahan” <oborahan@gmail.com>
2024-04-17 20:19:29 +0100

    commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,8 +11,15 @@ export default function Home() {
       </Head>
       <div className="flex h-screen justify-center items-center">
         <div className="text-center">
-          <h1 className="text-6xl font-bold">Bitronix</h1>
-          <p className="text-xl mt-2">Loading...</p>
+          <h1 className="text-7xl md:text-8xl font-extrabold leading-tighter tracking-tighter mb-10">
+            Bitronix
+          </h1>
+          <h2 className="text-4xl md:text-5xl font-extrabold leading-tighter tracking-tighter mb-10">
+            <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">
+              Empowering Tomorrow, One Byte at a Time!
+            </span>
+          </h2>
+          <p className="text-xl mt-2 font-semibold">Loading...</p>
         </div>
       </div>
       <Footer />
